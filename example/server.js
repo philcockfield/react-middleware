7823b7688a176f8334a3c2eb6752b89363a7ca48
Phil Cockfield <phil@cockfield.net>
2015-09-27 19:25:09 -0700

    Tidy up example

--- example/server.js
+++ example/server.js
@@ -1,7 +1,5 @@
-import _ from "lodash";
 import express from "express";
 import ReactServerPages from "../src";
-import chalk from "chalk";
 
 
 // ReactServerPages.start({ base:"./example/site" });
@@ -12,27 +10,6 @@ const middleware = ReactServerPages({ base:"./example/site" });
 
 // Create the folder-structure and base template files.
 // middleware.clearCache();
-middleware.templates.create();
-
-
-
-
-middleware.start();
-
-
-// const PORT = 8080;
-// app.listen(PORT, () => {
-//         const HR = _.repeat("-", 80)
-//         console.log("\n");
-//         console.log("React Server Pages (Example):");
-//         console.log(chalk.grey(HR));
-//         console.log(" - port:", chalk.cyan(PORT));
-//         console.log(" - env: ", process.env.NODE_ENV || "development");
-//         console.log(" - paths:");
-//         _.forIn(middleware.paths, (value, key) => {
-//           if (_.isString(value)) {
-//             console.log(`     - ${ chalk.magenta(key) }:`, chalk.grey(value));}
-//           }
-//         );
-//         console.log("");
-// });
+// middleware.templates.create();
+// middleware.start();
+middleware.init();
