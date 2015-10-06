cfb36d9a2bcedd51966293a44677f85ec4899963
Phil Cockfield <phil@cockfield.net>
2015-10-06 12:27:38 -0700

    quote marks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 "use strict"
 var gulp = require("gulp");
-var plumber = require('gulp-plumber');
+var plumber = require("gulp-plumber");
 var eslint = require("gulp-eslint");
 var babel = require("gulp-babel");
 var SOURCE_PATH = "./src/**/*.js";
