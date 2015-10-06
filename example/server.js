8a9cbbf477ea35c1e1c6edbf7bc36c823ee06d68
Phil Cockfield <phil@cockfield.net>
2015-10-06 10:01:33 -0700

    Show off start method on the root module.

--- example/server.js
+++ example/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import ReactMiddleware from "../src";
 
 
+ReactMiddleware.start({ base:"./example/site" });
 
-const middleware = ReactMiddleware({ base:"./example/site" });
-middleware.start();
+// const middleware = ReactMiddleware({ base:"./example/site" });
+// middleware.start();
