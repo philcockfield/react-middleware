7a83c957f9c7c08f4587468e78d057815b1d50c0
Phil Cockfield <phil@cockfield.net>
2015-10-26 20:20:07 +1300

    Copy and check version of react.

--- src/init.js
+++ src/init.js
@@ -13,21 +13,17 @@ const packageVersion = (dir) => packageJs(dir).version;
 const REACT_PATH = fsPath.join(__dirname, "../node_modules/react")
 const PARENT_REACT_PATH = fsPath.resolve("./node_modules/react");
 
-console.log("REACT_PATH", REACT_PATH);
-console.log("PARENT_REACT_PATH", PARENT_REACT_PATH);
-console.log("");
-
-// if (REACT_PATH !== PARENT_REACT_PATH) {
-//   if (fs.existsSync(PARENT_REACT_PATH)) {
-//     // Ensure matching version.
-//     const REQUIRED_VERSION = packageVersion(REACT_PATH);
-//     const CURRENT_VERSION = packageVersion(PARENT_REACT_PATH);
-//     if (REQUIRED_VERSION !== CURRENT_VERSION) {
-//       throw new Error(`Wrong version of 'react' installed. '${ REQUIRED_VERSION }' is required, the current version is '${ CURRENT_VERSION }'.`);
-//     }
-//   } else {
-//     // Copy the 'react' module into root.
-//     // NOTE: This is used by JSX files.
-//     fs.copySync(REACT_PATH, PARENT_REACT_PATH);
-//   }
-// }
+if (REACT_PATH !== PARENT_REACT_PATH) {
+  if (fs.existsSync(PARENT_REACT_PATH)) {
+    // Ensure matching version.
+    const REQUIRED_VERSION = packageVersion(REACT_PATH);
+    const CURRENT_VERSION = packageVersion(PARENT_REACT_PATH);
+    if (REQUIRED_VERSION !== CURRENT_VERSION) {
+      throw new Error(`Wrong version of 'react' installed. '${ REQUIRED_VERSION }' is required, the current version is '${ CURRENT_VERSION }'.`);
+    }
+  } else {
+    // Copy the 'react' module into root.
+    // NOTE: This is used by JSX files.
+    fs.copySync(REACT_PATH, PARENT_REACT_PATH);
+  }
+}
